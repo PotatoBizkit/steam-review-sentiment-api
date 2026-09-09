c339f0c1116259669b4d6a751c90b29ffd050767
Chetan Mudlapur <chetanx777@gmail.com>
2026-09-09 17:57:37 +0530

    feat: add sentiment prediction endpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from app.routers import sentiment
 
 app = FastAPI()
+
+app.include_router(sentiment.router)
 
 @app.get("/")
 def home():
--- app/routers/sentiment.py
+++ app/routers/sentiment.py
@@ -7,14 +7,14 @@ model = joblib.load("app/models/sentiment_model.pkl")
 vectorizer = joblib.load("app/models/tfidf_vectorizer.pkl")
 
 @router.post("/predict", response_model=SentimentOutput)
-def predict_sentiment(data: ReviewInput)
+def predict_sentiment(data: ReviewInput):
     txt_tfidf = vectorizer.transform([data.review_text])
     pred = model.predict(txt_tfidf)[0]
     prob = model.predict_proba(txt_tfidf)[0]
-    confidence = float(max(probability))
+    confidence = float(max(prob))
 
     return SentimentOutput(
-        review_text=payload.review_text,
-        recommendation=bool(prediction),
-        confidence=confidence
+        review_text = data.review_text,
+        recommendation = bool(pred),
+        confidence = confidence
     )

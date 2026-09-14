f9ccfc559a0897aa3e3e8c3e8f1752f11bdf7941
Chetan Mudlapur <chetanx777@gmail.com>
2026-09-14 15:13:53 +0530

    Create Table

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from app.routers import sentiment
+from app.database import Base, engine
+from app.models.predictions import Prediction
 
 app = FastAPI()
 
@@ -8,3 +10,5 @@ app.include_router(sentiment.router)
 @app.get("/")
 def home():
     return "S'up fellas!"
+
+Base.metadata.create_all(bind=engine)
